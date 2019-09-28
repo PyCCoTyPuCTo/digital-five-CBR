8e336250612c39ee65816484575ecb86c113e31f
BezlepkinAndrey <andbezlepkin@rambler.ru>
2019-09-28 14:51:00 +0300

    Добавил тип пользователю и поправил выборку записей в voite контроллере

---
+++ database/migrations/2019_09_28_113243_add_permition_relation.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPermitionRelation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_permission');
+            $table->foreign('id_permission')
+                ->references('id')
+                ->on('permissions')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
